bb7c8c09365d31a7dafcbf0d72fff6ac289ad4cd
yannafaith <yannafaith97@gmail.com>
2019-03-23 23:49:57 -0700

    connects graphql to demo databse

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,46 +1,27 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require('./generated/prisma-client');
 
-// dummy data stored in memory. Would be in a database.
-let links = [{
-    id: 'link-0',
-    url: 'howtographql.com',
-    description: 'Fullstack tutorial for Graphql'
-}];
-
-let idCount = links.length;
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews clone`,
-        feed: () => links,
-        link: (parent, args) => links.find(link => link.id === `link-${args.id}`),
+        feed: (root, args, context, info) => {
+            return context.prisma.links()
+        },
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
+        post: (root, args, context) => {
+            return context.prisma.createLink({
                 url: args.url,
-            };
-            links.push(link);
-            return link;
-        },
-        updateLink: (parent, args) => {
-            const link = links.find(link => link.id === `link-${args.id}`);
-            if (args.url) {link.url = args.url}
-            if (args.description) {link.description = args.description}
-            return link;
-        },
-        deleteLink: (parent, args) => {
-            links = links.filter(link => link.id !== `link-${args.id}`);
-            return `link-${args.id} deleted.`
-
+                description: args.description
+            })
         },
     },
 };
 
 const server = new GraphQLServer ({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: {prisma},
 });
 
 server.start(() => console.log('Server is running on http://localhost:4000'));
